cc542dcff9765a0f58f52f290ab906c33c5d0ddd
marta-ent <aw.martaclemente@gmail.com>
2024-12-15 19:23:16 +0100

    Create Saludo.java
    Añado Saludo.java con métodos de bienvenida y despedida

---
+++ Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+    public void saludo() {
+        System.out.println("¡Hola! Bienvenido");
+    }
+
+    public void despedida() {
+        System.out.println("¡Adiós!");
+    }
+}
